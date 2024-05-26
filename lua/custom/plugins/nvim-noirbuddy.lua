cbae0ed01eb833c2cc2535f5c864e3ab823f7239
yrammos <y@rammos.co>
2024-05-26 22:51:51 +0200

    Add nvim-noirbuddy theme.

---
+++ lua/custom/plugins/nvim-noirbuddy.lua
@@ -0,0 +1,20 @@
+return {
+  'jesseleite/nvim-noirbuddy',
+  dependencies = {
+    { 'tjdevries/colorbuddy.nvim' },
+  },
+  lazy = false,
+  priority = 1000,
+  opts = {
+    -- All of your `setup(opts)` will go here
+    preset = 'crt-green',
+    -- preset = 'kiwi',
+    -- preset = 'miami-nights',
+    -- preset = 'minimal',
+    -- preset = 'slate',
+    -- preset = 'crt-amber',
+  },
+  init = function()
+    vim.cmd.colorscheme 'noirbuddy'
+  end,
+}
