fbcbef57e5d03e9e6248244a36e9126d87b9ceee
yrammos <y@rammos.co>
2024-09-04 23:49:34 +0200

    Remove prettier and stick with prettierd.

--- init.lua
+++ init.lua
@@ -824,7 +824,8 @@ require('lazy').setup({
         python = { 'ruff' },
         markdown = { 'markdownlint' },
         latex = { 'latexindent' },
-        json = { 'prettierd', 'prettier' },
+        json = { 'prettierd' },
+        html = { 'prettierd' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
