9add519ad572a087b472f4251ea04be33e5250ae
yrammos <y@rammos.co>
2025-01-26 14:02:29 +0100

    Remove ultimate-autopair.

--- lua/custom/plugins/ultimate-autopair.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'altermo/ultimate-autopair.nvim',
-    event={'InsertEnter','CmdlineEnter'},
-    branch='v0.6', --recomended as each new version will have breaking changes
-    opts={
-        --Config goes here
-    },
-}
