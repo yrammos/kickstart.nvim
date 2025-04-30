85f71000bf9cf5734b7af023dcd25911a998d8eb
yrammos <y@rammos.co>
2025-04-30 07:28:01 +0200

    Remove copilot cmp following blink update.

--- init.lua
+++ init.lua
@@ -921,9 +921,10 @@ require('lazy').setup({
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'lazydev', 'copilot' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          copilot = { name = 'copilot', module = 'blink-cmp-copilot', score_offset = 100, async = true },
         },
       },
 
