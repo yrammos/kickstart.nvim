4d0f3ef51fbb23a0daabe56eb5f128898518301e
yrammos <y@rammos.co>
2025-08-10 01:23:52 +0200

    Ibid

--- lua/custom/plugins/ltex-client.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'icewind/ltex-client.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  opts = {
-    user_dictionaries_path = vim.env.HOME .. '/.local/share/nvim/',
-  },
-}
---
+++ lua/custom/plugins/ltex-utils-nvim.lua
@@ -0,0 +1,13 @@
+return {
+  'jhofscheier/ltex-utils.nvim',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-telescope/telescope.nvim',
+    -- "nvim-telescope/telescope-fzf-native.nvim", -- optional
+  },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
