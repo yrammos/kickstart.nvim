5a4bdf906ddf616d7279e6cc7249b287346b998f
yrammos <y@rammos.co>
2025-08-10 21:44:38 +0200

    Remove mini.sessions, mini.git, and mini.pairs. Replace mini.pairs with nvim-autopairs.

--- init.lua
+++ init.lua
@@ -1021,11 +1021,7 @@ require('lazy').setup({
           starterscreen.gen_hook.padding(3, 2),
         },
       }
-
-      require('mini.sessions').setup()
-      require('mini.git').setup()
       require('mini.diff').setup()
-      require('mini.pairs').setup()
     end,
   },
   { -- Highlight, edit, and navigate code
@@ -1069,7 +1065,7 @@ require('lazy').setup({
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
