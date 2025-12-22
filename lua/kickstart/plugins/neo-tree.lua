e118697f4be26807e05d8941ee78cb59c613a7b3
yrammos <y@rammos.co>
2025-12-22 21:29:45 +0100

    Fix neotree key mapping

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '\\', ':Neotree toggle<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
