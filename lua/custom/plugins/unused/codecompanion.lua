b7fa79f2f32617332de94d09028a3f8220980c5c
yrammos <y@rammos.co>
2025-12-22 21:30:45 +0100

    Add codecompanion spec in `unused` directory for future reference.

---
+++ lua/custom/plugins/unused/codecompanion.lua
@@ -0,0 +1,27 @@
+return {
+  'olimorris/codecompanion.nvim',
+  enable = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = {
+    send_code = false,
+    interactions = {
+      chat = {
+        adapter = 'copilot',
+      },
+      inline = {
+        adapter = 'copilot',
+      },
+      cmd = {
+        adapter = 'copilot',
+      },
+    },
+  },
+  keys = {
+    { '<leader>ca', '<cmd>CodeCompanionActions<cr>', { 'n', 'v' }, desc = '[C]odeCompanion [A]ctions' },
+    { '<leader>cc', '<cmd>CodeCompanionChat Toggle<cr>', { 'n', 'v' }, desc = '[C]odeCompanion [Chat]' },
+    { '<leader>cd', '<cmd>CodeCompanionChat Add<cr>', { 'v' }, desc = '[C]odeCompanion A[D]d' },
+  },
+}
