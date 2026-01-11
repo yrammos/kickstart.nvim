e0ffadff1cc51d64acc673711877f26d1941d73a
yrammos <y@rammos.co>
2026-01-11 02:16:28 +0100

    Add border to blink cmp signature popups

--- init.lua
+++ init.lua
@@ -739,7 +739,7 @@ require('lazy').setup({
       fuzzy = { implementation = 'lua' },
 
       -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
+      signature = { enabled = true, window = { border = 'single' } },
     },
   },
 
