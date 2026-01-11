e22d82ab1d5832e04d587581d393b4bc4338ae65
yrammos <y@rammos.co>
2026-01-11 02:18:52 +0100

    Format blink cmp completion window

--- init.lua
+++ init.lua
@@ -716,7 +716,10 @@ require('lazy').setup({
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = 'single' } },
+        menu = { border = 'single', draw = {
+          columns = { { 'kind_icon', gap = 2, 'label' }, { 'label_description', 'kind' } },
+        } },
       },
 
       sources = {
