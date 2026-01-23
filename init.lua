3884b2c977fb5f0bf62ddcdd4a443d89438727d6
yrammos <y@rammos.co>
2026-01-23 13:01:26 +0100

    Instruct ls_lua to stop complaining about the lack of ` vim` global.

--- init.lua
+++ init.lua
@@ -519,8 +519,7 @@ require('lazy').setup({
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { globals = { 'vim' }, disable = { 'missing-fields' } },
             },
           },
         },
