3caa76f2c663317ec1b5c7e3ca49162577a59c1a
yrammos <y@rammos.co>
2026-01-23 12:45:38 +0100

    Move texlab keymaps and engine selection scripts to ftplugin.

---
+++ ftplugin/plaintex.lua
@@ -0,0 +1,1 @@
+dofile(vim.fn.stdpath 'config' .. '/ftplugin/tex.lua')
---
+++ ftplugin/tex.lua
@@ -0,0 +1,37 @@
+local function set_engine_arg_to(arg)
+  local texlab_client = vim.lsp.get_clients({ name = 'texlab' })[1]
+  if texlab_client and texlab_client.config.settings.texlab.build.args then
+    texlab_client.config.settings.texlab.build.args[4] = arg
+    texlab_client.notify('workspace/didChangeConfiguration', {
+      settings = texlab_client.config.settings,
+    })
+    print('TeXlab engine set to `' .. arg .. '`.')
+  else
+    print 'Unable to find active TeXlab LSP client.'
+  end
+end
+
+vim.api.nvim_buf_create_user_command(0, 'TexlabPDFLaTeX', function()
+  set_engine_arg_to '-pdf'
+end, { desc = 'Set engine to pdfLaTeX' })
+
+vim.api.nvim_buf_create_user_command(0, 'TexlabXeLaTeX', function()
+  set_engine_arg_to '-xelatex'
+end, { desc = 'Set engine to XeLaTeX' })
+
+local map = function(keys, func, desc, mode)
+  mode = mode or 'n'
+  vim.keymap.set(mode, keys, func, {
+    buffer = true, -- Crucial: only active in this TeX buffer
+    silent = true,
+    desc = 'TeXLab: ' .. desc,
+  })
+end
+
+map('<localleader>ll', '<cmd>LspTexlabBuild<cr>', 'Bui[l]d')
+map('<localleader>lk', '<cmd>LspTexlabCancelBuild<cr>', 'Cancel Build')
+map('<localleader>lv', '<cmd>LspTexlabForward<cr>', 'Forward Search')
+map('<localleader>la', '<cmd>LspTexlabCleanAuxiliary<cr>', 'Clean Aux')
+map('<localleader>lA', '<cmd>LspTexlabCleanArtifacts<cr>', 'Clean Artifacts')
+map('<localleader>le', '<cmd>LspTexlabChangeEnvironment<cr>', 'Change Env')
+map('<localleader>lf', '<cmd>LspTexlabFindEnvironments<cr>', 'Find Env')
