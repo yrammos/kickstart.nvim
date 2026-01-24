8816f8cf352cce62fea23fafdb821be485e30e13
yrammos <y@rammos.co>
2026-01-25 00:46:13 +0100

    Stop highlighting the current line.

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -25,7 +25,7 @@ return {
       signs_staged_enable = true,
       signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
       numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir = {
         follow_files = true,
